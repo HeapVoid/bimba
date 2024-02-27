9c7519c83a219108369a1186b0643dfbe65841bb
HeapVoid <heapvoid@gmail.com>
2024-02-27 04:19:07 +0300

    utils from ts to js

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-function colorize(text:string, fg?:number, bg?:number) {
+function colorize(text, fg, bg) {
     let result = ''
     if(fg) result += "\x1b[38;5;" + fg.toString() + "m"
     if(bg) result += "\x1b[48;5;" + bg.toString() + "m"
@@ -10,21 +10,21 @@ function colorize(text:string, fg?:number, bg?:number) {
 // theme for messages printed in terminal
 // https://i.stack.imgur.com/KTSQa.png
 export const theme = {
-    code: (text:string) =>      colorize(text, 252, 238),
-    margin: (text:string) =>    colorize(text, 229, 145),
-    error: (text:string) =>     colorize(text, 196 , 52),
-    ecode: (text:string) =>     colorize(text, 196, 238),
-    action: (text:string) =>    colorize(text, 237),
-    folder: (text:string) =>    colorize(text, 240),
-    filename: (text:string) =>  colorize(text, 15),
-    success: (text:string) =>   colorize(text, 40),
-    failure: (text:string) =>   colorize(text, 15, 124),
+    code: (text) =>      colorize(text, 252, 238),
+    margin: (text) =>    colorize(text, 229, 145),
+    error: (text) =>     colorize(text, 196 , 52),
+    ecode: (text) =>     colorize(text, 196, 238),
+    action: (text) =>    colorize(text, 237),
+    folder: (text) =>    colorize(text, 240),
+    filename: (text) =>  colorize(text, 15),
+    success: (text) =>   colorize(text, 40),
+    failure: (text) =>   colorize(text, 15, 124),
 
-    flags: (text:string) =>      colorize(text, 5),
-    count: (text:string) =>      colorize(text, 15),
-    start: (text:string) =>      colorize(text, 252, 233),
-    filedir: (text:string) =>    colorize(text, 15),
-    time: (text:string) =>       colorize(text, 41),
-    link: (text:string) =>       colorize(text, 15),
-    online: (text:string) =>     colorize(text, 40, 22)
+    flags: (text) =>      colorize(text, 5),
+    count: (text) =>      colorize(text, 15),
+    start: (text) =>      colorize(text, 252, 233),
+    filedir: (text) =>    colorize(text, 15),
+    time: (text) =>       colorize(text, 41),
+    link: (text) =>       colorize(text, 15),
+    online: (text) =>     colorize(text, 40, 22)
 }
